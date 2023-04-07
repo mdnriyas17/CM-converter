0a78012b0b723a932bb1c8d370ef2a09b5088e38
mdnriyas17 <mdnriyas@gmail.com>
2023-04-07 12:02:39 +0530

    update footer

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ function handlechange(){
     let Centemeter =  Number(document.getElementById("input").value)
 let convertinch = Centemeter /2.54
 let result = document.getElementById("value");
-result.innerHTML= `${convertinch.toFixed(2)}inch`;
+result.innerHTML= `${convertinch.toFixed(2)} inch`;
 }
